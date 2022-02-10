69a137d6323b6f07b63e655f6f26d15e9a90826d
Georgy Aleksahin <g.aleksahin@idaproject.com>
2022-02-10 20:54:39 +0300

    Простое веб приложение на ноде

---
+++ web/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Test')
+});
+
+app.get('/test', (req, res) => {
+    res.send('Test')
+});
+
+app.listen(8000, () => {
+    console.log('Port 8000')
+})
