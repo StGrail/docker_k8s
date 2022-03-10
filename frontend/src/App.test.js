6c843463644c25ad298c930d586965e3b43d7f39
Georgy Aleksahin <g.aleksahin@idaproject.com>
2022-03-10 16:39:43 +0300

    feat(frontend_app): добавил запуск тестов при изменении и перезапуске фронта

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('ещё 1 тест', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
